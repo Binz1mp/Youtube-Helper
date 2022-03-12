0cb09085c8247768d396c4922056ffc1338e73d4
Binz1mp <96367152+Binz1mp@users.noreply.github.com>
2022-03-12 13:29:41 +0900

    temporary solution, will be improved soon

--- script_main/content-script.js
+++ script_main/content-script.js
@@ -8,51 +8,30 @@
 
 
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     var checkButton = document.getElementById('magicbutton');
-//     checkButton.addEventListener('click', function () {
-//         document.body.style.backgroundColor = 'red';
-//         alert("엘렐레");
-//     })
-//   })
+// function collapsibleElement (){
+//     const injectElement = document.createElement('div');
+//     injectElement.innerHTML = '<button type="button" class="related-collapse">Open Collapsible</button>';
+//     document.querySelector("#meta").appendChild(injectElement);
+// }
 
 
+// setTimeout(collapsibleElement, 2000);
 
-// var toggle = false;
-// chrome.runtime.onMessage.addListener(function() {
-//   toggle = !toggle;
-//   Array.prototype.forEach.call(document.querySelectorAll('#related'), function(el) {
-//     el.className = el.className + "begone";
+
+// var coll = document.getElementsByClassName("related-collapse");
+// var i;
+
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     alert("1");
+//     var targetStuff = document.querySelector("#related");
+//     alert("2");
+//     if (targetStuff.style.display === "") {
+//         targetStuff.style.display = "none";
+//     } else {
+//         targetStuff.style.display = "";
+//     }
+//     alert("3");
 //   });
-// });
-
-
-
-
-function collapsibleElement (){
-    const injectElement = document.createElement('div');
-    injectElement.innerHTML = '<button type="button" class="related-collapse">Open Collapsible</button>';
-    document.querySelector("#meta").appendChild(injectElement);
-}
-
-
-setTimeout(collapsibleElement, 2000);
-
-
-var coll = document.getElementsByClassName("related-collapse");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    alert("1");
-    var targetStuff = document.querySelector("#related");
-    alert("2");
-    if (targetStuff.style.display === "") {
-        targetStuff.style.display = "none";
-    } else {
-        targetStuff.style.display = "";
-    }
-    alert("3");
-  });
-}
+// }
