8cb92219435dc00cc522a7ad5309b14002079dd5
Binz1mp <96367152+Binz1mp@users.noreply.github.com>
2022-03-14 16:42:28 +0900

    collapse function added

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 # Youtube-Related-Videos-Remover
 toy project no.1, Chrome extension
 
-Sometimes you may want to remove or disable some related videos and see comments first. This does that.
-
 still in progress, stuck in collapse function.
-
--- script_main/content-script.js
+++ script_main/content-script.js
@@ -1,49 +1,20 @@
+document.addEventListener('yt-navigate-finish', function collapsibleElement (){
+    const injectElement = document.createElement('div');
+    let collapseButton = document.getElementsByClassName("related-collapse");
+    var i;
 
-// function somestuff () {
-//     var targetStuff = document.querySelector("#related");
-//     setTimeout(targetStuff.style.display = "none", 1000);
-// }
+    injectElement.innerHTML = '<button type="button" class="related-collapse">Collapse Related Videos</button>';
+    document.querySelector("#meta").appendChild(injectElement);
 
-// setTimeout(somestuff, 1000);
-
-
-
-// function collapsibleElement (){
-//     const injectElement = document.createElement('div');
-//     injectElement.innerHTML = '<button type="button" class="related-collapse">Open Collapsible</button>';
-//     document.querySelector("#meta").appendChild(injectElement);
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// setTimeout(collapsibleElement, 2000);
-
-
-// var coll = document.getElementsByClassName("related-collapse");
-// var i;
-
-// for (i = 0; i < coll.length; i++) {
-//   coll[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     alert("1");
-//     var targetStuff = document.querySelector("#related");
-//     alert("2");
-//     if (targetStuff.style.display === "") {
-//         targetStuff.style.display = "none";
-//     } else {
-//         targetStuff.style.display = "";
-//     }
-//     alert("3");
-//   });
-// }
+    for (i = 0; i < collapseButton.length; i++) {
+        collapseButton[i].addEventListener("click", function () {
+          this.classList.toggle("active");
+          var targetStuff = document.querySelector("#related");
+          if (targetStuff.style.display === "") {
+              targetStuff.style.display = "none";
+          } else {
+              targetStuff.style.display = "";
+          }
+        });
+      };
+});
