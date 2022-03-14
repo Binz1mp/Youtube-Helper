ca52893f049d587bb44eab25401c2434240b67bd
Binz1mp <96367152+Binz1mp@users.noreply.github.com>
2022-03-14 21:39:32 +0900

    text change as you click the button added

--- script_main/content-script.js
+++ script_main/content-script.js
@@ -2,21 +2,22 @@ document.addEventListener('yt-navigate-finish', function collapsibleElement (){
     const injectElement = document.createElement('div');
     let collapseButton = document.getElementsByClassName("related-collapse");
     var i;
-    injectElement.innerHTML = '<button type="button" class="related-collapse">Collapse Related Videos 2</button>';
+    injectElement.innerHTML = '<button type="button" class="related-collapse">Close Related Videos</button>';
     document.querySelector("#meta").appendChild(injectElement);
     for (i = 0; i < collapseButton.length; i++) {
         collapseButton[i].addEventListener("click", function () {
-          this.classList.toggle("active");
           var targetStuff = document.querySelector("#related");
           // var fulltarget = document.querySelector("#related").style.display;
           if (targetStuff.style.display === "") {
               targetStuff.style.display = "none";
               localStorage.setItem("related_display", 'display_none');
+              document.querySelector(".related-collapse").textContent = 'Open Related Videos';
               console.log("버튼클릭 로컬스토리지 = " + localStorage.getItem('related_display'));
           } else {
               targetStuff.style.display = "";
               localStorage.setItem("related_display", 'display_normal');
               console.log("버튼클릭 로컬스토리지 = " + localStorage.getItem('related_display'));
+              document.querySelector(".related-collapse").textContent = 'Close Related Videos';
           }
         });
         
