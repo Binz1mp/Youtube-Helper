f059ebf9c3a009a86ff03e5929d203eeece1ef23
Binz1mp <96367152+Binz1mp@users.noreply.github.com>
2022-04-20 09:34:49 +0900

    2022-04-20 09:20 - version 1.1.9.4

--- script_main/content-script.js
+++ script_main/content-script.js
@@ -356,9 +356,9 @@ document.addEventListener('yt-navigate-finish', function collapsibleElement() {
     };
 
     for (i = 0; i < topButton.length; i++) {
-            topButton[i].addEventListener('click', function () {
+            topButton[i].addEventListener('click', function (e) {
                 if (window.location.href.indexOf('youtube.com/watch') > -1) {
-                preventDefault();
+                e.preventDefault();
                 window.scrollTo({top: 0, behavior: 'smooth'});
             } else {
                 console.log("top button not found");
