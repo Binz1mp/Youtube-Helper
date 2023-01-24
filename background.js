441fa431df440a18d96690cb1cd7e109165ee69b
Binz1mp <binieon@naver.com>
2023-01-25 08:26:13 +0900

    fixed update pop when chrome update, not extension update.

--- background.js
+++ background.js
@@ -1,16 +1,33 @@
-chrome.runtime.onInstalled.addListener(function () {
+chrome.runtime.onInstalled.addListener(function (details) {
   var lang = "undefined";
   if(navigator.language !=null) {
     lang = navigator.language;
   }
-  lang = lang.toLocaleLowerCase().substring(0.2);
-  if (lang == "ko") {
-    chrome.tabs.create({
-      url: chrome.runtime.getURL("changelog/update_ko.html"),
-    });
-  } else {
-    chrome.tabs.create({
-      url: chrome.runtime.getURL("changelog/update_en.html"),
-    });
+  lang = lang.toLocaleLowerCase().substring(0,2);
+  // if (lang == "ko") {
+  //   chrome.tabs.create({
+  //     url: chrome.runtime.getURL("changelog/update_ko.html"),
+  //   });
+  // } else {
+  //   chrome.tabs.create({
+  //     url: chrome.runtime.getURL("changelog/update_en.html"),
+  //   });
+  // }
+
+  // const versionCur = chrome.runtime.getManifest().version;
+  // const versionPrev = details.previousVersion;
+  const reason = details.reason;
+  switch(reason) {
+    // case 'install': {  }
+    case 'update':
+      if (lang == "ko") {
+        chrome.tabs.create({
+          url: chrome.runtime.getURL("changelog/update_ko.html"),
+        });
+      } else {
+        chrome.tabs.create({
+          url: chrome.runtime.getURL("changelog/update_en.html"),
+        });
+      }
   }
 });
