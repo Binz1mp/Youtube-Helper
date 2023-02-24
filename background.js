baa8e78242670d60338a3bc12862da8b9bb4dfab
Binz1mp_Laptop <you@example.com>
2023-02-25 07:40:12 +0900

    install and update detact test

--- background.js
+++ background.js
@@ -4,21 +4,20 @@ chrome.runtime.onInstalled.addListener(function (details) {
     lang = navigator.language;
   }
   lang = lang.toLocaleLowerCase().substring(0,2);
-  // if (lang == "ko") {
-  //   chrome.tabs.create({
-  //     url: chrome.runtime.getURL("changelog/update_ko.html"),
-  //   });
-  // } else {
-  //   chrome.tabs.create({
-  //     url: chrome.runtime.getURL("changelog/update_en.html"),
-  //   });
-  // }
-
-  // const versionCur = chrome.runtime.getManifest().version;
-  // const versionPrev = details.previousVersion;
   const reason = details.reason;
   switch(reason) {
     // case 'install': {  }
+    case 'install':
+      if (lang == "ko") {
+        chrome.tabs.create({
+          url: chrome.runtime.getURL("changelog/update_ko.html"),
+        });
+      } else {
+        chrome.tabs.create({
+          url: chrome.runtime.getURL("changelog/update_en.html"),
+        });
+      }
+      break;
     case 'update':
       if (lang == "ko") {
         chrome.tabs.create({
@@ -29,5 +28,8 @@ chrome.runtime.onInstalled.addListener(function (details) {
           url: chrome.runtime.getURL("changelog/update_en.html"),
         });
       }
+      break;
+    case 'chrome_update': break;
+    case 'shared_module_update': break;
   }
 });
